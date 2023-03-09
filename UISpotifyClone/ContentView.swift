f0905ba7ea1fbb83f76a396f02f55bbdb779f8f6
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-09 10:50:34 -0300

    CHORE: beginning of cover effect

--- UISpotifyClone/ContentView.swift
+++ UISpotifyClone/ContentView.swift
@@ -8,14 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let bgColor = Color(hue: 0.0, saturation: 0.0, brightness: 0.071)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView(showsIndicators: false) {
+            /// Album cover art
+            GeometryReader { reader in
+                let offSetY =  getOffSetY(reader: reader)
+                let height: CGFloat = (reader.size.height - offSetY) + offSetY / 3
+                let minHeight: CGFloat = 120
+                let opacity =  (height - minHeight) / (reader.size.height - minHeight)
+
+                ZStack {
+                    LinearGradient(gradient: Gradient(colors: [Color.green, Color.black.opacity(0.0)]), startPoint: .top, endPoint: .bottom)
+                        .scaleEffect(7)
+
+                    Image("cover")
+                        .resizable()
+                        .frame(width: height,
+                        height: height)
+                        .offset(y: offSetY)
+                        .opacity(opacity)
+                        .shadow(color: .black.opacity(0.5), radius: 30)
+                }
+                .frame(width: reader.size.width)
+            }
+            .frame(height: 250)
+
+            // Test scroll
+            ForEach(0..<100) { _ in
+                Rectangle()
+                    .foregroundColor(.white)
+                    .frame(height: 100)
+                    .opacity(0.3)
+            }
         }
-        .padding()
+        .coordinateSpace(name: CoordinateNames.scrollView.rawValue)
+        .background(bgColor.ignoresSafeArea())
+    }
+
+    private func getOffSetY(reader: GeometryProxy) -> CGFloat {
+        let offSetY: CGFloat = -reader.frame(in: .named(CoordinateNames.scrollView)).minY
+
+        if offSetY < 0 {
+            return offSetY / 1.3
+        }
+        return offSetY
+    }
+}
+extension ContentView {
+    enum CoordinateNames: String, Hashable {
+        case scrollView
     }
 }
 
