daf2201a84489e943ac65d165d62183904ab59f5
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-03-09 09:50:28 -0300

    Initial Commit

---
+++ UISpotifyClone/UISpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UISpotifyCloneApp.swift
+//  UISpotifyClone
+//
+//  Created by Luiz Araujo on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct UISpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
